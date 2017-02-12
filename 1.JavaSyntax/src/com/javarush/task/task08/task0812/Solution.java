510c8f3ceb582e9c44ba655c2dc429b26bc2d867
vladmeh <vladmeh@gmail.com>
2017-02-13 02:11:57 +0300

    level08.lecture06

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -2,8 +2,6 @@ package com.javarush.task.task08.task0812;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 /* 
 Cамая длинная последовательность
@@ -11,7 +9,7 @@ Cамая длинная последовательность
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
             list.add(Integer.parseInt(reader.readLine()));
@@ -23,38 +21,13 @@ public class Solution {
         for (int i = 1; i < list.size(); i++) {
             if (list.get(i) == list.get(i-1))
                 len++;
-
-            if (len > max && list.get(i) != list.get(i-1)){
-                max = len;
+            else
                 len = 1;
-            }
+
+            if (len > max)
+                max = len;
         }
 
         System.out.println(max);
-
-        /*int max = 1;
-        int muk = 1;
-        for (int i = 0; i < list.size() - 1; i++) {
-
-            while (list.get(i) == list.get(i + 1)) {
-                max++;
-                i++;
-                if (i + 1 == list.size()) {
-                    if (max > muk) {
-                        System.out.println(max);
-                        return;
-                    } else {
-                        System.out.println(muk);
-                        return;
-                    }
-                }
-            }
-            if (max > muk) {
-                muk = max;
-                max = 1;
-            }
-        }
-        System.out.println(muk);*/
-
     }
 }
