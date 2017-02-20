32a638f7e2617d24c242c61b30203c6d035e1aa9
VladMeh <vladmeh@gmail.com>
2017-02-20 13:44:08 +0300

    level09.lecture06

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0906/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task09.task0906;
+
+/* 
+Логирование стек трейса
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        log("In main method");
+    }
+
+    public static void log(String s) {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        System.out.println(stackTraceElements[2].getClassName() + ": " + stackTraceElements[2].getMethodName() + ": " + s);
+    }
+}
