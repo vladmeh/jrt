a2d4dab6faaed6058d0ef6ae8cbdf451e83c565f
VladMeh <vladmeh@gmail.com>
2017-03-13 15:48:22 +0300

    questcore.level12.lecture12

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1233/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task12.task1233;
+
+/* 
+Задача по алгоритмам
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndIndex(data);
+
+        System.out.println("Minimum is " + result.x);
+        System.out.println("Index of minimum element is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        Integer y = 0, x = array[y];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < x){
+                x = array[i];
+                y = i;
+            }
+        }
+
+        return new Pair<Integer, Integer>(x, y);
+    }
+
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
