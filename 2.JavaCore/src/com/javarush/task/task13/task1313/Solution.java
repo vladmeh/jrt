3f951fa95e43a8024578681f18a6258b340eef49
vladmeh <vladmeh@gmail.com>
2017-03-15 01:57:32 +0300

    questcore.level13.lecture04

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1313/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task13.task1313;
+
+import java.awt.*;
+
+/* 
+Интерфейс Animal
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public static abstract class Fox implements Animal{
+
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
