f89785ddff0c661d5d3df8d32ac0becd9fa53d17
vladmeh <vladmeh@gmail.com>
2017-03-26 21:36:27 +0300

    questcore.level15.lecture12

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task15.task1523;
+
+import java.util.Date;
+
+/**
+ * Created by vlad on 26.03.2017.
+ */
+public class SubSolution extends Solution {
+    protected SubSolution(String s) {
+        super(s);
+    }
+
+    SubSolution(Integer n) {
+        super(n);
+    }
+
+    public SubSolution(Date date) {
+        super(date);
+    }
+}
