348fed97c9b330ee074f22322c0b6af0bf479555
VladMeh <vladmeh@gmail.com>
2017-04-21 17:56:04 +0300

    questcore.level18.lecture11

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1818/SolutionTest.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task18.task1818;
+
+/**
+ * Created by mvl on 21.04.2017.
+ */
+public class SolutionTest {
+}
---
+++ Java4thEdition/src/com/javarush/task/level18/DirList3.java
@@ -0,0 +1,7 @@
+package com.javarush.task.level18;
+
+/**
+ * Created by mvl on 21.04.2017.
+ */
+public class DirList3 {
+}
