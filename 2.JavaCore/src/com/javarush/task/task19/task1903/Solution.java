fbf89c87ee8276a0531a017a929fc07e16dfdf1e
VladMeh <vladmeh@gmail.com>
2017-04-27 12:53:02 +0300

    questcore.level19.lecture03

--- 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -2,8 +2,27 @@ package com.javarush.task.task19.task1903;
 
 /* 
 Адаптация нескольких интерфейсов
+Адаптируй IncomeData к Customer и Contact.
+Классом-адаптером является IncomeDataAdapter.
+Инициализируйте countries перед началом выполнения программы. Соответствие кода страны и названия:
+UA Ukraine
+RU Russia
+CA Canada
+Дополнить телефонный номер нулями до 10 цифр при необходимости (смотри примеры).
+Обратите внимание на формат вывода фамилии и имени человека.
+
+
+Требования:
+1. Класс Solution должен содержать public static поле countries типа Map.
+2. В статическом блоке класса Solution инициализируй поле countries тестовыми данными согласно заданию.
+3. Класс IncomeDataAdapter должен реализовывать интерфейсы Customer и Contact.
+4. Класс IncomeDataAdapter должен содержать приватное поле data типа IncomeData.
+5. Класс IncomeDataAdapter должен содержать конструктор с параметром IncomeData.
+6. В классе IncomeDataAdapter реализуй методы интерфейсов Customer и Contact используя подсказки в виде комментариев в интерфейсах.
 */
 
+import java.text.DecimalFormat;
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,22 +48,37 @@ public class Solution {
 
         @Override
         public String getCompanyName() {
-            return data.getCompany();
+            return data.getCompany(); //example JavaRush Ltd.
         }
 
         @Override
         public String getCountryName() {
-            return data.getContactLastName();
+            return countries.get(data.getCountryCode()); //example Ukraine
         }
 
         @Override
         public String getName() {
-            return data.getContactFirstName();
+            return data.getContactLastName() + ", " + data.getContactFirstName(); //example Ivanov, Ivan
         }
 
         @Override
         public String getPhoneNumber() {
-            return String.valueOf(data.getPhoneNumber());
+            int phoneFmt = data.getPhoneNumber();
+            //10 цифр в строке, заполнение слева '0' (по префиксу)
+            DecimalFormat phoneDecimalFmt = new DecimalFormat("0000000000");
+            String phoneRawString= phoneDecimalFmt.format(phoneFmt);
+
+            //example (050)123-45-67
+            MessageFormat phoneMsgFmt = new MessageFormat("({0}){1}-{2}-{3}");
+            //С учетом группировки 3-3-2-2
+            String[] phoneNumArr={
+                    phoneRawString.substring(0, 3),
+                    phoneRawString.substring(3, 6),
+                    phoneRawString.substring(6, 8),
+                    phoneRawString.substring(8)
+            };
+
+            return String.format("+%d%s", data.getCountryPhoneCode(), phoneMsgFmt.format(phoneNumArr));
         }
     }
 
---
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task19.task1904;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/* 
+И еще один адаптер
+
+Адаптировать Scanner к PersonScanner.
+Классом-адаптером является PersonScannerAdapter.
+В классе адаптере создать приватное финальное поле Scanner fileScanner. Поле инициализировать в конструкторе с одним аргументом типа Scanner.
+
+Данные в файле хранятся в следующем виде:
+Иванов Иван Иванович 31 12 1950
+Петров Петр Петрович 31 12 1957
+
+В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read() должен читать данные только одного человека.
+
+
+Требования:
+1. PersonScanner должен быть интерфейсом.
+2. Класс PersonScannerAdapter должен реализовывать интерфейс PersonScanner.
+3. Класс PersonScannerAdapter должен содержать приватное поле fileScanner типа Scanner.
+4. Класс PersonScannerAdapter должен содержать конструктор с параметром Scanner.
+5. Метод close() класса PersonScannerAdapter должен делегировать полномочие такому же методу fileScanner.
+6. Метод read() класса PersonScannerAdapter должен читать строку с файла, парсить её, и возвращать данные только одного человека, в виде объекта класса Person.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class PersonScannerAdapter implements PersonScanner{
+        private final Scanner fileScanner;
+
+        public PersonScannerAdapter(Scanner fileScanner) {
+            this.fileScanner = fileScanner;
+        }
+
+        @Override
+        public Person read() throws IOException {
+            String[] aLine = fileScanner.nextLine().split(" ");
+            String lastName = aLine[0]; //Фамилия
+            String firstName = aLine[1]; //Имя
+            String middleName = aLine[2]; //Отчество
+
+            SimpleDateFormat format = new SimpleDateFormat("dd MM yyyy");
+            Date birthDate = new Date();
+            try {
+                birthDate = format.parse(aLine[3] + " " + aLine[4] + " " + aLine[5]);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return new Person( firstName, middleName, lastName, birthDate);
+        }
+
+        @Override
+        public void close() throws IOException {
+            fileScanner.close();
+        }
+    }
+}
