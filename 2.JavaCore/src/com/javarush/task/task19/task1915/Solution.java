39e5e80cc4435168957488a93757e1d63bc714e0
vladmeh <vladmeh@gmail.com>
2017-04-28 02:00:20 +0300

    questcore.level19.lecture08

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -0,0 +1,80 @@
+package com.javarush.task.task19.task1915;
+
+/* 
+Дублируем текст
+
+Считай с консоли имя файла.
+В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна дублировать вывод всего текста в файл, имя которого ты считал.
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+Закрой поток файла.
+
+Пример вывода на экран:
+it's a text for testing
+
+Пример тела файла:
+it's a text for testing
+
+
+Требования:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку "it's a text for testing".
+5. В методе main(String[] args) программа должна считывать имена файлов с консоли (используй BufferedReader).
+6. В методе main(String[] args) BufferedReader для считывания данных с консоли должен быть закрыт.
+7. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c параметром конструктора ByteArrayOutputStream).
+8. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль объекта System.out.
+9. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+10. Метод main(String[] args) класса Solution должен записывать в файл строку выведенную методом printSomething() (используй FileOutputStream), и дублировать её в консоль.
+11. Поток записи в файл (FileOutputStream) должен быть закрыт.
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException {
+        //запоминаем настоящий PrintStream в специальную переменную
+        PrintStream consoleStream = System.out;
+
+        //Создаем динамический массив
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //создаем адаптер к классу PrintStream
+        PrintStream stream = new PrintStream(outputStream);
+        //Устанавливаем его как текущий System.out
+        System.setOut(stream);
+
+        //Вызываем функцию, которая ничего не знает о наших манипуляциях
+        testString.printSomething();
+
+        //Преобразовываем записанные в наш ByteArray данные в строку
+        String result = outputStream.toString();
+
+        //Возвращаем все как было
+        System.setOut(consoleStream);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        /*BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileName));
+        fileWriter.write(result);
+        fileWriter.close();*/
+
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        fileOutputStream.write(result.getBytes());
+        fileOutputStream.close();
+
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
