29746546faa3c3ea6ad2ed619e73e28dfb072fd9
vladmeh <vladmeh@gmail.com>
2017-05-05 04:00:25 +0300

    questcore.level19.lecture11

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -13,12 +13,15 @@ public class Solution {
     public static void main(String[] args) {
         String tmp = "Info about Leela <span xml:lang=»en» lang=»en»><b><span>Turanga Leela</span></b></span><span>Super</span><span>girl</span>";
 
-        Pattern p = Pattern.compile("<(span(.)*?)>(?=.*?(<\\1>))(?!.*<\\1)", Pattern.MULTILINE);
+        Pattern p = Pattern.compile("<span(.)*?>(?=(?:.*<(\\1)>)?)</span>", Pattern.MULTILINE);
+        Pattern p1 = Pattern.compile("<([^> ]+)>(?=(?:.*<(\\1)>)?)", Pattern.MULTILINE);
+        Pattern p2 = Pattern.compile("<(span(.)*?)>\\p{all}?</span>", Pattern.MULTILINE);
+        Pattern p3 = Pattern.compile("(<[^> /!-]+>)(?![\\s\\S]*\\1)", Pattern.MULTILINE);
 
-        Matcher m = p.matcher(tmp);
+        Matcher m = p3.matcher(tmp);
         List<String> exclude = new ArrayList<String>();
         while ( m.find() ) {
-            System.out.println(m.group(3));
+            System.out.println(m.group());
             /*if ( exclude.indexOf( m.group(1).toLowerCase() ) == -1 ) {
                 System.out.println( m.group() );
             }*/
