7fafedc37580a88830bc1d841e37d1f7235f16ff
VladMeh <vladmeh@gmail.com>
2017-05-24 15:34:55 +0300

    questmultithreading.level22.lecture18 - 6

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -4,4 +4,18 @@ package com.javarush.task.task23.task2312;
  * Created by mvl on 24.05.2017.
  */
 public class SnakeSection {
+    private int x, y;
+
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
