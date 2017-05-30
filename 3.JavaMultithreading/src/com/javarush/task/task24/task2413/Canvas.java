6a74779866fb465e2afeae3693b11d6137e90549
VladMeh <vladmeh@gmail.com>
2017-05-30 17:53:32 +0300

    questmultithreading.level03.lecture13. Arkanoid - 9

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -4,4 +4,36 @@ package com.javarush.task.task24.task2413;
  * @autor mvl on 30.05.2017.
  */
 public class Canvas {
+    private int width, height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height+2][width+2];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
 }
