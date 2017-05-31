faed744a31801e870835a32e853056edd628bd55
VladMeh <vladmeh@gmail.com>
2017-05-31 13:04:01 +0300

    questmultithreading.level03.lecture13. Arkanoid - 13

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -4,8 +4,14 @@ package com.javarush.task.task24.task2413;
  * Created by mvl on 30.05.2017.
  */
 public class Ball extends BaseObject{
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed, direction, dx, dy;
+    private boolean isFrozen;
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        this.isFrozen = true;
     }
 
     @Override
@@ -16,5 +22,21 @@ public class Ball extends BaseObject{
     @Override
     public void move() {
 
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
     }
 }
