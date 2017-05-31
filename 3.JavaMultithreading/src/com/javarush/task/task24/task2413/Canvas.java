fd73a95cec139189d44355783a5e088ec2d59f76
VladMeh <vladmeh@gmail.com>
2017-05-31 12:51:57 +0300

    questmultithreading.level03.lecture13. Arkanoid - 11

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -36,6 +36,21 @@ public class Canvas {
         }
     }
 
+    public void clear(){
+        this.matrix = new char[height+2][width+2];
+    }
+
+    public void print(){
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[0].length; j++)
+            {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     public int getWidth() {
         return width;
     }
