a1a409097089ab20561a7fea851432f6d2e76439
VladMeh <vladmeh@gmail.com>
2017-05-31 12:53:49 +0300

    questmultithreading.level03.lecture13. Arkanoid - 12

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -4,8 +4,8 @@ package com.javarush.task.task24.task2413;
  * Created by mvl on 30.05.2017.
  */
 public class Brick extends BaseObject {
-    public Brick(double x, double y, double radius) {
-        super(x, y, radius);
+    public Brick(double x, double y) {
+        super(x, y, 3);
     }
 
     @Override
