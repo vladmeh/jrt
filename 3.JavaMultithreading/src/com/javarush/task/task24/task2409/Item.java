429152678a31c940b5ac16425fd7bd31ee8e833a
VladMeh <vladmeh@gmail.com>
2017-06-02 12:38:16 +0300

    questmultithreading.level24.lecture06

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * @autor mvl on 02.06.2017.
+ */
+public interface Item {
+    int getId();
+    double getPrice();
+    String getTM();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * @autor mvl on 02.06.2017.
+ */
+public interface Jeans extends Item {
+    int getLength();
+    int getSize();
+}
