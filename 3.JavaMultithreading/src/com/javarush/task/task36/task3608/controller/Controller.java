a061fbf91d2edd5015315abc004670372cc8b37f
VladMeh <vladmeh@gmail.com>
2017-06-05 16:00:14 +0300

    questmultithreading.level24.lecture14. MVC(2)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task36.task3608.controller;
+
+/**
+ * @autor mvl on 05.06.2017.
+ */
+public class Controller {
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/View.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task36.task3608.view;
+
+/**
+ * @autor mvl on 05.06.2017.
+ */
+public interface View {
+}
