e62856c88ea0179ed7295c428fea5551a03f81e5
VladMeh <vladmeh@gmail.com>
2017-06-05 16:25:56 +0300

    questmultithreading.level24.lecture14. MVC(2)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+import java.util.List;
+
+/**
+ * @autor mvl on 05.06.2017.
+ */
+public class UsersView implements View {
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("All users:");
+        List<User> users = modelData.getUsers();
+
+        for (User user: users)
+            System.out.println("\t" + user);
+
+        System.out.println("==================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void fireEventShowAllUsers(){
+        controller.onShowAllUsers();
+    }
+}
