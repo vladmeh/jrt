974f591ec93c88cdbfd41c1a6201b04eff85a750
VladMeh <vladmeh@gmail.com>
2017-06-05 16:00:21 +0300

    questmultithreading.level24.lecture14. MVC(2)

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -1,7 +1,18 @@
 package com.javarush.task.task36.task3608.controller;
 
+import com.javarush.task.task36.task3608.model.Model;
+
 /**
  * @autor mvl on 05.06.2017.
  */
 public class Controller {
+    private Model model;
+
+    public void onShowAllUsers(){
+        model.loadUsers();
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/View.java
@@ -1,7 +1,13 @@
 package com.javarush.task.task36.task3608.view;
 
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
 /**
  * @autor mvl on 05.06.2017.
  */
 public interface View {
+    void refresh(ModelData modelData);
+
+    void setController(Controller controller);
 }
