f21cf2c31353f39cdc53f10eb346559f8de8cb9f
VladMeh <vladmeh@gmail.com>
2017-06-06 15:21:25 +0300

    questmultithreading.level25.lecture02

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() {
+            if (loadWheelNamesFromDB().length != 4){
+                throw new  IllegalArgumentException();
+            }else {
+                wheels = new ArrayList<>();
+                for (String s: loadWheelNamesFromDB())
+                    wheels.add(Wheel.valueOf(s));
+            }
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+        new Car();
+    }
+}
