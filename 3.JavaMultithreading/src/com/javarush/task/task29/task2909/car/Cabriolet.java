a622a8930656e4db180b61c4fbfe9bee87002c27
VladMeh <vladmeh@gmail.com>
2017-06-13 12:59:09 +0300

    questmultithreading.level25.lecture16. Refactoring(10).

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Cabriolet.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task29.task2909.car;
+
+/**
+ * @autor mvl on 13.06.2017.
+ */
+public class Cabriolet extends Car {
+    public Cabriolet(int numberOfPassengers) {
+        super(Car.CABRIOLET, numberOfPassengers);
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Sedan.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task29.task2909.car;
+
+/**
+ * @autor mvl on 13.06.2017.
+ */
+public class Sedan extends Car{
+    public Sedan(int numberOfPassengers) {
+        super(Car.SEDAN, numberOfPassengers);
+    }
+}
