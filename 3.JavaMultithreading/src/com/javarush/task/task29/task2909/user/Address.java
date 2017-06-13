5f3b87800437d077ca904abcec9613034ac5d82e
VladMeh <vladmeh@gmail.com>
2017-06-13 16:02:19 +0300

    questmultithreading.level25.lecture16. Refactoring(14-15).

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/Address.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task29.task2909.user;
+
+/**
+ * @autor mvl on 13.06.2017.
+ */
+public class Address {
+    private String country;
+    private String city;
+    private String house;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -4,12 +4,9 @@ public class User {
     private String name;
     private String surname;
     private int age;
-
-    private String country;
-    private String city;
-    private House house;
-
     private Work work;
+    private boolean man;
+    private Address address;
 
     public User(String name, String surname, int age) {
         this.name = name;
@@ -27,6 +24,10 @@ public class User {
             System.out.println("Пользователь моложе 16 лет");
         else
             System.out.println("Пользователь старше 16 лет");
+    }
+
+    public String getBoss(){
+        return work.getBoss();
     }
 
     public String getName() {
@@ -54,23 +55,23 @@ public class User {
     }
 
     public String getCountry() {
-        return country;
+        return address.getCountry();
     }
 
     public void setCountry(String country) {
-        this.country = country;
+        address.setCountry(country);
     }
 
     public String getCity() {
-        return city;
+        return address.getCity();
     }
 
     public void setCity(String city) {
-        this.city = city;
+        address.setCity(city);
     }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
     }
 
     public Work getWork() {
@@ -79,5 +80,13 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
     }
 }
