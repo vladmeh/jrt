4fc597a53b3a118cbcba50ef52e1e4fadbfb94cd
VladMeh <vladmeh@gmail.com>
2017-06-14 12:12:03 +0300

    questmultithreading.level26.lecture02

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2601/Solution.java
@@ -7,20 +7,27 @@ import java.util.Arrays;
 */
 public class Solution {
     public static void main(String[] args) {
+        Integer[] array = {13,8,15,5,17};
+        System.out.println(Arrays.toString(array));
 
+        System.out.println(Arrays.toString(sort(array)));
     }
 
     public static Integer[] sort(Integer[] array) {
+        //Сортируем массив
         Arrays.sort(array);
-
+        System.out.println(Arrays.toString(array));
+        //Находим медиану
         int i_middle = array.length/2;
         final double mediana = (array.length % 2 == 1) ? array[i_middle] : (array[i_middle - 1] + array[i_middle]) / 2;
 
+        System.out.println("медиана - " + (int)mediana);
+
+        //Сортируем массив по удаленности его отмедианы
         Arrays.sort(array, (o1, o2) -> {
             int result = (int)Math.round(Math.abs(mediana - o1) - Math.abs(mediana - o2));
             return (result == 0) ? (o1 - o2) : result;
         });
-
         return array;
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task26.task2603;
+
+import java.util.Comparator;
+
+/*
+Убежденному убеждать других не трудно
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class CustomizedComparator<T> implements Comparator<T>{
+        private Comparator<T>[] comparators;
+
+        public CustomizedComparator(Comparator<T>... vararg ) {
+            this.comparators = vararg;
+        }
+
+
+        @Override
+        public int compare(T o1, T o2) {
+            int result = 0;
+            for (Comparator<T> comparator : comparators) {
+                result = comparator.compare(o1, o2);
+                if (result != 0)
+                    break;
+            }
+            return result;
+        }
+    }
+}
