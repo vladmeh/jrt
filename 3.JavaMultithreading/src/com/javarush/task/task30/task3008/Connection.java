dd13bc73229e593967a4d63afa215be26871c7c7
VladMeh <vladmeh@gmail.com>
2017-06-15 14:29:18 +0300

    questmultithreading.level26.lecture15. Chat(5)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,7 +1,44 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.*;
+import java.net.*;
+
 /**
  * @autor mvl on 15.06.2017.
  */
-public class Connection {
+public class Connection implements Closeable{
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        // Создать объект класса ObjectOutputStream нужно до того, как будет создаваться объект
+        // класса ObjectInputStream, иначе может возникнуть взаимная блокировка потоков,
+        // которые хотят установить соединение
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void send(Message message) throws IOException {
+        synchronized (out){
+            out.writeObject(message);
+        }
+    }
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        synchronized (in){
+            return (Message)in.readObject();
+        }
+    }
+
+    public SocketAddress getRemoteSocketAddress(){
+        return socket.getRemoteSocketAddress();
+    }
+
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
 }
