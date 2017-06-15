de48c36a6816c36e7e8a38b5efee9d3d8fe2c15f
VladMeh <vladmeh@gmail.com>
2017-06-15 17:09:24 +0300

    questmultithreading.level26.lecture15. Chat(12)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+
+/**
+ * @autor mvl on 15.06.2017.
+ */
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread{
+
+    }
+}
