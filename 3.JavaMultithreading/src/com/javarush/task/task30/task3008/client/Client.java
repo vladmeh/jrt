0715a7f3d4c9d387ea67c4c84cd5aa7898009d3d
VladMeh <vladmeh@gmail.com>
2017-06-15 17:47:50 +0300

    questmultithreading.level26.lecture15. Chat(14)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -11,8 +11,52 @@ public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
 
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
     public class SocketThread extends Thread{
 
+    }
+
+    public void run(){
+        // Создавать новый сокетный поток с помощью метода getSocketThread.
+        SocketThread socketThread = getSocketThread();
+
+        // Помечать созданный поток как daemon, это нужно для того,
+        // чтобы при выходе из программы вспомогательный поток прервался автоматически.
+        socketThread.setDaemon(true);
+
+        // Запустить вспомогательный поток.
+        socketThread.start();
+
+        // Заставить текущий поток ожидать, пока он не получит нотификацию из другого потока.
+        synchronized (this) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                ConsoleHelper.writeMessage("Выход из программы по ошибке.");
+            }
+        }
+
+        //проверь значение clientConnected
+        if (clientConnected)
+            ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+        else
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+
+        // Считывай сообщения с консоли пока клиент подключен.
+        while (clientConnected){
+            String text = ConsoleHelper.readString();
+            // Если будет введена команда ‘exit‘, то выйди из цикла.
+            if (text.toLowerCase().equals("exit")) break;
+
+            // После каждого считывания, если метод shouldSendTextFromConsole() возвращает true,
+            // отправь считанный текст с помощью метода sendTextMessage().
+            if (shouldSendTextFromConsole())
+                sendTextMessage(text);
+        }
     }
 
     protected String getServerAddress(){
