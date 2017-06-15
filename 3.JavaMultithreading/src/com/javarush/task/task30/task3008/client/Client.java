658cbc4dae82cdb04384905f7ededec423c3f1ec
VladMeh <vladmeh@gmail.com>
2017-06-15 17:20:09 +0300

    questmultithreading.level26.lecture15. Chat(13)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task30.task3008.client;
 
-import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.*;
+
+import java.io.IOException;
 
 /**
  * @autor mvl on 15.06.2017.
@@ -11,5 +13,37 @@ public class Client {
 
     public class SocketThread extends Thread{
 
+    }
+
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Введите адрес сервера: ");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Введите порт сервера: ");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Введите имя пользователя: ");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Ошибка при отправке сообщения. Соединение будет закрыто.");
+            clientConnected = false;
+        }
     }
 }
