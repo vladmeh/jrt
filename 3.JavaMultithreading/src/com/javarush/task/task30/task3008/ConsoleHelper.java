5523aeb5fedbdb3908756d5b43c0934f0d5887d6
VladMeh <vladmeh@gmail.com>
2017-06-15 12:50:04 +0300

    questmultithreading.level26.lecture15. Chat(2).

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,7 +1,41 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @autor mvl on 15.06.2017.
  */
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString(){
+        String line = null;
+        while (line == null){
+            try { line = reader.readLine();}
+            catch (IOException e) {
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return line;
+    }
+
+    public static int readInt(){
+        int number;
+        while (true){
+            try {
+                number = Integer.parseInt(readString());
+                break;
+            }
+            catch (NumberFormatException e) {
+                writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return number;
+    }
 }
