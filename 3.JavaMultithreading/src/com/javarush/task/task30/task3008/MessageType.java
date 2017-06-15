2dbf6e0c09c49a2bd308d8e893e7d4a7d2015f56
VladMeh <vladmeh@gmail.com>
2017-06-15 13:57:28 +0300

    questmultithreading.level26.lecture15. Chat(3).

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
@@ -4,4 +4,10 @@ package com.javarush.task.task30.task3008;
  * @autor mvl on 15.06.2017.
  */
 public enum MessageType {
+    NAME_REQUEST, //запрос имени
+    USER_NAME, //имя пользователя
+    NAME_ACCEPTED, //имя принято
+    TEXT, //текстовое сообщение
+    USER_ADDED, //пользователь добавлен
+    USER_REMOVED //пользователь удален
 }
