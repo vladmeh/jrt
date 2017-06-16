36ab640de261255d58e1ed067672e8ab879f3446
VladMeh <vladmeh@gmail.com>
2017-06-16 15:35:40 +0300

    questmultithreading.level26.lecture15. Chat(19)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,5 +1,11 @@
 package com.javarush.task.task30.task3008.client;
 
+import com.javarush.task.task30.task3008.ConsoleHelper;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * @autor mvl on 16.06.2017.
  */
@@ -10,7 +16,55 @@ public class BotClient extends Client {
     }
 
     public class BotSocketThread extends Client.SocketThread{
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
+        @Override
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+            String[] messageParts = message.split(": ");
+            if (messageParts.length == 2) {
+                String messageAuthor = messageParts[0];
+                String messageText = messageParts[1].toLowerCase();
+                String dateTimeformat = null;
+                switch (messageText) {
+                    case "дата":
+                        dateTimeformat = "d.MM.YYYY";
+                        break;
+                    case "день":
+                        dateTimeformat = "d";
+                        break;
+                    case "месяц":
+                        dateTimeformat = "MMMM";
+                        break;
+                    case "год":
+                        dateTimeformat = "YYYY";
+                        break;
+                    case "время":
+                        dateTimeformat = "H:mm:ss";
+                        break;
+                    case "час":
+                        dateTimeformat = "H";
+                        break;
+                    case "минуты":
+                        dateTimeformat = "m";
+                        break;
+                    case "секунды":
+                        dateTimeformat = "s";
+                        break;
+                }
+                if (dateTimeformat != null) {
+                    String reply = String.format("Информация для %s: %s",
+                            messageAuthor,
+                            new SimpleDateFormat(dateTimeformat).format(Calendar.getInstance().getTime())
+                    );
+                    sendTextMessage(reply);
+                }
+            }
+        }
     }
 
     @Override
