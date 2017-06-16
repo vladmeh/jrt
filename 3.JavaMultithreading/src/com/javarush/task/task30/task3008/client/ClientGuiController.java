5970a66a0164e4f6bfa3055f52a1d1572a7079bd
VladMeh <vladmeh@gmail.com>
2017-06-16 16:31:38 +0300

    questmultithreading.level26.lecture15. Chat(21)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/ClientGuiController.java
@@ -0,0 +1,74 @@
+package com.javarush.task.task30.task3008.client;
+
+/**
+ * @autor mvl on 16.06.2017.
+ */
+public class ClientGuiController extends Client {
+    private ClientGuiModel model;
+    private ClientGuiView view;
+
+    public ClientGuiController() {
+        this.model = new ClientGuiModel();
+        this.view = new ClientGuiView(this);
+    }
+
+    public class GuiSocketThread extends Client.SocketThread{
+        @Override
+        protected void processIncomingMessage(String message) {
+            model.setNewMessage(message);
+            view.refreshMessages();
+        }
+
+        @Override
+        protected void informAboutAddingNewUser(String userName) {
+            model.addUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        protected void informAboutDeletingNewUser(String userName) {
+            model.deleteUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            view.notifyConnectionStatusChanged(clientConnected);
+        }
+    }
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new GuiSocketThread();
+    }
+
+    @Override
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.run();
+    }
+
+    @Override
+    protected String getServerAddress() {
+        return view.getServerAddress();
+    }
+
+    @Override
+    protected int getServerPort() {
+        return view.getServerPort();
+    }
+
+    @Override
+    protected String getUserName() {
+        return view.getUserName();
+    }
+
+    public ClientGuiModel getModel(){
+        return model;
+    }
+
+    public static void main(String[] args) {
+        ClientGuiController controller = new ClientGuiController();
+        controller.run();
+    }
+}
