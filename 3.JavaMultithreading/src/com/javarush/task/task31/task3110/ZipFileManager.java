f935d4610265fa613a6863576f500071759d4b07
VladMeh <vladmeh@gmail.com>
2017-06-20 17:55:53 +0300

    questmultithreading.level27.lecture15. Archiver(18)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -129,51 +129,55 @@ public class ZipFileManager {
         Files.move(tempZipFile, zipFile, StandardCopyOption.REPLACE_EXISTING);
     }
 
-    public void addFile(Path absolutePath) throws Exception
-    {
+    public void addFile(Path absolutePath) throws Exception {
         addFiles(Collections.singletonList(absolutePath));
     }
 
-    public void addFiles(List<Path> absolutePathList) throws Exception{
+    public void addFiles(List<Path> absolutePathList) throws Exception {
+        // Проверяем существует ли zip файл
         if (!Files.isRegularFile(zipFile)) {
             throw new WrongZipFileException();
         }
 
+        // Создаем временный файл
         Path tempZipFile = Files.createTempFile(null, null);
-
         List<Path> archiveFiles = new ArrayList<>();
 
-        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(tempZipFile)))
-        {
-            try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile)))
-            {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(tempZipFile))) {
+            try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+
                 ZipEntry zipEntry = zipInputStream.getNextEntry();
-                while (zipEntry != null)
-                {
+                while (zipEntry != null) {
                     String fileName = zipEntry.getName();
                     archiveFiles.add(Paths.get(fileName));
+
                     zipOutputStream.putNextEntry(new ZipEntry(fileName));
                     copyData(zipInputStream, zipOutputStream);
+
                     zipInputStream.closeEntry();
                     zipOutputStream.closeEntry();
+
                     zipEntry = zipInputStream.getNextEntry();
                 }
             }
-            for (Path file : absolutePathList)
-            {
+
+            // Архивируем новые файлы
+            for (Path file : absolutePathList) {
                 if (Files.isRegularFile(file))
                 {
                     if (archiveFiles.contains(file.getFileName()))
                         ConsoleHelper.writeMessage(String.format("Файл '%s' уже существует в архиве.", file.toString()));
-                    else
-                    {
+                    else {
                         addNewZipEntry(zipOutputStream, file.getParent(), file.getFileName());
                         ConsoleHelper.writeMessage(String.format("Файл '%s' добавлен в архиве.", file.toString()));
                     }
-                } else
+                }
+                else
                     throw new PathIsNotFoundException();
             }
         }
+
+        // Перемещаем временный файл на место оригинального
         Files.move(tempZipFile, zipFile, StandardCopyOption.REPLACE_EXISTING);
     }
 
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
@@ -7,23 +7,22 @@ import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/**
- * @autor mvl on 20.06.2017.
- */
 public class ZipAddCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-        try
-        {
+        try {
             ConsoleHelper.writeMessage("Добавление нового файла в архив.");
+
             ZipFileManager zipFileManager = getZipFileManager();
+
             ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
             Path sourcePath = Paths.get(ConsoleHelper.readString());
+
             zipFileManager.addFile(sourcePath);
+
             ConsoleHelper.writeMessage("Добавление в архив завершено.");
-        }
-        catch (PathIsNotFoundException e)
-        {
+
+        } catch (PathIsNotFoundException e) {
             ConsoleHelper.writeMessage("Файл не был найден.");
         }
     }
