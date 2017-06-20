3aa316f39681b350644f0e6b9bb70350df2e7935
VladMeh <vladmeh@gmail.com>
2017-06-20 12:23:17 +0300

    questmultithreading.level27.lecture15. Archiver(2)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @autor mvl on 20.06.2017.
@@ -13,6 +19,17 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception{
+        try(
+            ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+            InputStream inputStream = Files.newInputStream(source)
+        ){
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
 
+            int c;
+            while ((c = inputStream.read()) != -1){
+                zipOutputStream.write(c);
+            }
+        }
     }
 }
