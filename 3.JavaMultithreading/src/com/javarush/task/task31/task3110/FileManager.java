82b1e063138aaab29ffc8eae05f7b878dc2b9be4
VladMeh <vladmeh@gmail.com>
2017-06-20 14:59:05 +0300

    questmultithreading.level27.lecture15. Archiver(10)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -7,35 +7,36 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @autor mvl on 20.06.2017.
- */
 public class FileManager {
     private Path rootPath;
-    private List<Path> fileList = new ArrayList<>();
+    private List<Path> fileList;
 
-    public FileManager(Path rootPath) throws IOException{
+    public FileManager(Path rootPath) throws IOException {
         this.rootPath = rootPath;
+        this.fileList = new ArrayList<>();
         collectFileList(rootPath);
     }
 
+    public List<Path> getFileList() {
+        return fileList;
+    }
+
     private void collectFileList(Path path) throws IOException {
+        // Добавляем только файлы
         if (Files.isRegularFile(path)) {
             Path relativePath = rootPath.relativize(path);
             fileList.add(relativePath);
         }
 
-        if (Files.isDirectory(path)){
-            try(DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path))
-            {
-                for (Path file: directoryStream) {
+        // Добавляем содержимое директории
+        if (Files.isDirectory(path)) {
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
+                for (Path file : directoryStream) {
                     collectFileList(file);
                 }
             }
         }
-    }
-
-    public List<Path> getFileList() {
-        return fileList;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,10 +1,14 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -19,17 +23,54 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception{
-        try(
-            ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
-            InputStream inputStream = Files.newInputStream(source)
-        ){
-            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
-            zipOutputStream.putNextEntry(zipEntry);
+        Path zipDirectory = zipFile.getParent();
+        if (Files.notExists(zipDirectory))
+            Files.createDirectories(zipDirectory);
 
-            int c;
-            while ((c = inputStream.read()) != -1){
-                zipOutputStream.write(c);
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+
+            if (Files.isDirectory(source)) {
+                // Если архивируем директорию, то нужно получить список файлов в ней
+                FileManager fileManager = new FileManager(source);
+                List<Path> fileNames = fileManager.getFileList();
+
+                // Добавляем каждый файл в архив
+                for (Path fileName : fileNames)
+                    addNewZipEntry(zipOutputStream, source, fileName);
+
+            } else if (Files.isRegularFile(source)) {
+
+                // Если архивируем отдельный файл, то нужно получить его директорию и имя
+                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
+            } else {
+
+                // Если переданный source не директория и не файл, бросаем исключение
+                throw new PathIsNotFoundException();
             }
+        }
+    }
+
+    private void addNewZipEntry(
+            ZipOutputStream zipOutputStream,
+            Path filePath,
+            Path fileName) throws Exception{
+
+        try (
+                InputStream inputStream = Files.newInputStream(filePath.resolve(fileName))
+        )
+        {
+            ZipEntry entry = new ZipEntry(fileName.toString());
+            zipOutputStream.putNextEntry(entry);
+
+            copyData(inputStream, zipOutputStream);
+            zipOutputStream.closeEntry();
+        }
+    }
+
+    private void copyData(InputStream in, OutputStream out) throws Exception{
+        int c;
+        while ((c = in.read()) != -1){
+            out.write(c);
         }
     }
 }
