3b3ef837876160753d090abaed926404b17c2f7e
VladMeh <vladmeh@gmail.com>
2017-06-20 16:44:33 +0300

    questmultithreading.level27.lecture15. Archiver(13)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
@@ -1,11 +1,9 @@
 package com.javarush.task.task31.task3110;
 
-/**
- * @autor mvl on 20.06.2017.
- */
 public class FileProperties {
     private String name;
-    private long size, compressedSize;
+    private long size;
+    private long compressedSize;
     private int compressionMethod;
 
     public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
@@ -15,26 +13,6 @@ public class FileProperties {
         this.compressionMethod = compressionMethod;
     }
 
-    public long getCompressionRatio(){
-        return 100 - ((compressedSize * 100) / size);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append(name);
-        if (size > 0)
-        {
-            builder.append("\t");
-            builder.append(size / 1024);
-            builder.append(" Kb (");
-            builder.append(compressedSize / 1024);
-            builder.append(" Kb) сжатие: ");
-            builder.append(getCompressionRatio());
-            builder.append("%");
-        }
-        return builder.toString();
-    }
 
     public String getName() {
         return name;
@@ -50,5 +28,28 @@ public class FileProperties {
 
     public int getCompressionMethod() {
         return compressionMethod;
+    }
+
+    public long getCompressionRatio() {
+        // Вычисляем степень сжатия
+        return 100 - ((compressedSize * 100) / size);
+    }
+
+    @Override
+    public String toString() {
+        // Строим красивую строку из свойств
+        StringBuilder builder = new StringBuilder();
+        builder.append(name);
+        if (size > 0) {
+            builder.append("\t");
+            builder.append(size / 1024);
+            builder.append(" Kb (");
+            builder.append(compressedSize / 1024);
+            builder.append(" Kb) сжатие: ");
+            builder.append(getCompressionRatio());
+            builder.append("%");
+        }
+
+        return builder.toString();
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,13 +1,18 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
@@ -67,6 +72,30 @@ public class ZipFileManager {
         int len;
         while ((len = in.read(buffer)) > 0) {
             out.write(buffer, 0, len);
+        }
+    }
+
+    public List<FileProperties> getFilesList() throws Exception{
+        if (!Files.isRegularFile(zipFile))
+            throw new WrongZipFileException();
+
+        List<FileProperties> filePropertiesList = new ArrayList<>();
+
+        try(ZipInputStream in = new ZipInputStream(Files.newInputStream(zipFile))){
+            ZipEntry zipEntry = in.getNextEntry();
+
+            while (zipEntry != null){
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                copyData(in, baos);
+                FileProperties fileProperties = new FileProperties(zipEntry.getName(),
+                        zipEntry.getSize(),
+                        zipEntry.getCompressedSize(),
+                        zipEntry.getMethod());
+                filePropertiesList.add(fileProperties);
+                zipEntry = in.getNextEntry();
+            }
+
+            return filePropertiesList;
         }
     }
 }
