b35d375c420c1947d586b6c8bf3a7c47d2d150d1
VladMeh <vladmeh@gmail.com>
2017-06-20 16:54:21 +0300

    questmultithreading.level27.lecture15. Archiver(14)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -3,7 +3,6 @@ package com.javarush.task.task31.task3110;
 import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -54,6 +53,32 @@ public class ZipFileManager {
         }
     }
 
+    public List<FileProperties> getFilesList() throws Exception {
+        // Проверяем существует ли zip файл
+        if (!Files.isRegularFile(zipFile)) {
+            throw new WrongZipFileException();
+        }
+
+        List<FileProperties> files = new ArrayList<>();
+
+        try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
+
+            while (zipEntry != null) {
+                // Поля "размер" и "сжатый размер" не известны, пока элемент не будет прочитан
+                // Давайте вычитаем его в какой-то выходной поток
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                copyData(zipInputStream, baos);
+
+                FileProperties file = new FileProperties(zipEntry.getName(), zipEntry.getSize(), zipEntry.getCompressedSize(), zipEntry.getMethod());
+                files.add(file);
+                zipEntry = zipInputStream.getNextEntry();
+            }
+        }
+
+        return files;
+    }
+
     private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
         Path fullPath = filePath.resolve(fileName);
         try (InputStream inputStream = Files.newInputStream(fullPath)) {
@@ -72,30 +97,6 @@ public class ZipFileManager {
         int len;
         while ((len = in.read(buffer)) > 0) {
             out.write(buffer, 0, len);
-        }
-    }
-
-    public List<FileProperties> getFilesList() throws Exception{
-        if (!Files.isRegularFile(zipFile))
-            throw new WrongZipFileException();
-
-        List<FileProperties> filePropertiesList = new ArrayList<>();
-
-        try(ZipInputStream in = new ZipInputStream(Files.newInputStream(zipFile))){
-            ZipEntry zipEntry = in.getNextEntry();
-
-            while (zipEntry != null){
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                copyData(in, baos);
-                FileProperties fileProperties = new FileProperties(zipEntry.getName(),
-                        zipEntry.getSize(),
-                        zipEntry.getCompressedSize(),
-                        zipEntry.getMethod());
-                filePropertiesList.add(fileProperties);
-                zipEntry = in.getNextEntry();
-            }
-
-            return filePropertiesList;
         }
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -1,11 +1,29 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.FileProperties;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Path;
+import java.util.List;
+
 /**
  * @autor mvl on 20.06.2017.
  */
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
 
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
+        List<FileProperties> filePropertiesList = zipFileManager.getFilesList();
+
+        for (FileProperties fileProperties: filePropertiesList)
+            ConsoleHelper.writeMessage(fileProperties.toString());
+
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
