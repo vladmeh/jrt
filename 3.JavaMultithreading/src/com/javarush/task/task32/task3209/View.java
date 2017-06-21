2f5b7f7b7d87426954cdf9fb4ed2cc40552011e8
VladMeh <vladmeh@gmail.com>
2017-06-21 16:07:17 +0300

    questmultithreading.level28.lecture15. HTML Editor(2)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -22,4 +22,10 @@ public class View extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
     }
+
+    public void init(){};
+
+    public void exit(){
+        controller.exit();
+    }
 }
