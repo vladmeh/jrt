0a5ca810bc53d710f18daa10930b70ba474c4ccf
VladMeh <vladmeh@gmail.com>
2017-06-21 16:22:50 +0300

    questmultithreading.level28.lecture15. HTML Editor(3)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/FrameListener.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task32.task3209.listeners;
+
+import com.javarush.task.task32.task3209.View;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @autor mvl on 21.06.2017.
+ */
+public class FrameListener extends WindowAdapter {
+    private View view;
+
+    public FrameListener(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        view.exit();
+        super.windowClosing(e);
+    }
+}
