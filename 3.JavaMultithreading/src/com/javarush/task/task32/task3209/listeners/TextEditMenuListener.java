23e27e749d6b5f9c21e43be836f496baed71580f
VladMeh <vladmeh@gmail.com>
2017-06-22 12:47:45 +0300

    questmultithreading.level28.lecture15. HTML Editor(13)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
@@ -2,19 +2,27 @@ package com.javarush.task.task32.task3209.listeners;
 
 import com.javarush.task.task32.task3209.View;
 
+import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
+import java.awt.*;
 
 /**
  * @autor mvl on 21.06.2017.
  */
 public class TextEditMenuListener implements MenuListener {
+    private View view;
+
     public TextEditMenuListener(View view) {
+        this.view = view;
     }
 
     @Override
     public void menuSelected(MenuEvent e) {
-
+        JMenu menu = (JMenu) e.getSource();
+        Component[] components = menu.getMenuComponents();
+        for (Component component: components)
+            component.setEnabled(view.isHtmlTabSelected());
     }
 
     @Override
