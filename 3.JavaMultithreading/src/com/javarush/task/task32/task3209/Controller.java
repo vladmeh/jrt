9c839d61a73a7e2d1e2d74c65f1e555c63144abb
VladMeh <vladmeh@gmail.com>
2017-06-22 14:35:31 +0300

    questmultithreading.level28.lecture15. HTML Editor(15)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task32.task3209;
 
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 /**
@@ -31,5 +32,14 @@ public class Controller {
 
     public HTMLDocument getDocument() {
         return document;
+    }
+
+    public void resetDocument(){
+        if (document != null)
+            document.removeUndoableEditListener(view.getUndoListener());
+
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(view.getUndoListener());
+        view.update();
     }
 }
