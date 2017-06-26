e0d9c090f5857936a5362ae5dff8c0542f7f0f4b
VladMeh <vladmeh@gmail.com>
2017-06-26 16:45:36 +0300

    questmultithreading.level29.lecture15. Restaurant (7).

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -4,4 +4,5 @@ package com.javarush.task.task27.task2712.ad;
  * @autor mvl on 26.06.2017.
  */
 public class AdvertisementManager {
+    AdvertisementStorage storage = AdvertisementStorage.getInstance();
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,15 +1,32 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @autor mvl on 26.06.2017.
  */
 public class AdvertisementStorage {
     private static AdvertisementStorage ourInstance = new AdvertisementStorage();
 
+    private final List<Advertisement> videos = new ArrayList<>();
+
     public static AdvertisementStorage getInstance() {
         return ourInstance;
     }
 
     private AdvertisementStorage() {
+        Object someContent = new Object();
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60));   //10 min
+    }
+
+    public List<Advertisement> list(){
+        return videos;
+    }
+
+    public void add(Advertisement advertisement){
+        videos.add(advertisement);
     }
 }
