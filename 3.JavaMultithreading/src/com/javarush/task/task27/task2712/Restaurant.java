0af2e8e05150154abd83b51d7fd20de3ecd9239e
VladMeh <vladmeh@gmail.com>
2017-07-03 11:35:20 +0300

    questmultithreading.level29.lecture15. Restaurant (19)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -2,6 +2,10 @@ package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
 import com.javarush.task.task27.task2712.kitchen.Waiter;
+import com.javarush.task.task27.task2712.statistic.StatisticManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @autor mvl on 26.06.2017.
@@ -12,12 +16,32 @@ public class Restaurant {
     public static void main(String[] args) {
 
         Waiter waiter = new Waiter();
-        Cook cook = new Cook("Amigo");
-        cook.addObserver(waiter);
+        Cook cookAmigo = new Cook("Amigo");
+        cookAmigo.addObserver(waiter);
 
-        Tablet tablet = new Tablet(5);
+        /*Tablet tablet = new Tablet(5);
         tablet.addObserver(cook);
-        tablet.createOrder();
+        tablet.createOrder();*/
+
+        Cook cookDiego = new Cook("Diego");
+        StatisticManager.getInstance().register(cookAmigo);
+        StatisticManager.getInstance().register(cookDiego);
+
+        List<Tablet> tablets = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Tablet tablet = new Tablet(i + 1);
+            tablet.addObserver(cookAmigo);
+            tablet.addObserver(cookDiego);
+            tablets.add(tablet);
+        }
+
+        Thread randomOrderGeneratorTaskThread = new Thread(new RandomOrderGeneratorTask(tablets, ORDER_CREATING_INTERVAL));
+        randomOrderGeneratorTaskThread.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+        randomOrderGeneratorTaskThread.interrupt();
 
         DirectorTablet directorTablet = new DirectorTablet();
         directorTablet.printAdvertisementProfit();
