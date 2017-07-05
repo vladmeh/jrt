7ce5467f9a45c933fb2178b16a14f4a8ca185d24
VladMeh <vladmeh@gmail.com>
2017-07-05 16:58:50 +0300

    questmultithreading.level10.lecture15. 2048(1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * @autor mvl on 05.07.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * @autor mvl on 05.07.2017.
+ */
+public class Model {
+}
