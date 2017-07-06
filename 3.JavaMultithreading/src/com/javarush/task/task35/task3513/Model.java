6d13e6f6a0f290ffa0ec6546ca287cb02e8f3e75
VladMeh <vladmeh@gmail.com>
2017-07-06 12:06:19 +0300

    questmultithreading.level30.lecture15. 2048(4)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @autor mvl on 05.07.2017.
  */
@@ -8,10 +11,33 @@ public class Model {
     private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
 
     public Model() {
+        resetGameTiles();
+    }
+
+    public void resetGameTiles() {
         for (int i = 0; i < FIELD_WIDTH; i++) {
             for (int j = 0; j < FIELD_WIDTH; j++) {
                 gameTiles[i][j] = new Tile();
             }
         }
+        addTile();
+        addTile();
+    }
+
+    private void addTile() {
+        List<Tile> emptyTiles = getEmptyTiles();
+        emptyTiles.get((int) (Math.random() * emptyTiles.size())).value
+                = ((Math.random() < 0.9) ? 2 : 4);
+    }
+
+    private List<Tile> getEmptyTiles() {
+        List<Tile> emptyTiles = new ArrayList<>();
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                Tile tile = gameTiles[i][j];
+                if (tile.isEmpty()) emptyTiles.add(tile);
+            }
+        }
+        return emptyTiles;
     }
 }
