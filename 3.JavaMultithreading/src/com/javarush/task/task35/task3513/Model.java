c36acf1b02d3d5b4d13b92cf6582571b3ae512c4
VladMeh <vladmeh@gmail.com>
2017-07-06 11:41:05 +0300

    questmultithreading.level10.lecture15. 2048(3)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -4,4 +4,14 @@ package com.javarush.task.task35.task3513;
  * @autor mvl on 05.07.2017.
  */
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+    public Model() {
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
