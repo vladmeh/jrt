232259b56a984a7a11c10daadafe4fde847fa7c6
VladMeh <vladmeh@gmail.com>
2017-07-11 17:48:43 +0300

    questcollections.level31.lecture04

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task31.task3112;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+/* 
+Загрузчик файлов
+*/
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        Path passwords = downloadFile("https://www.amigo.com/ship/secretPassword.txt", Paths.get("D:/MyDownloads"));
+
+        for (String line : Files.readAllLines(passwords)) {
+            System.out.println(line);
+        }
+
+    }
+
+    public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
+        String fileName = urlString.substring(urlString.lastIndexOf("/")+1);
+        Path resultFile = Paths.get(downloadDirectory + "/" + fileName);
+
+        URL url = new URL(urlString);
+        InputStream is = url.openStream();
+
+        Path tempFile = Files.createTempFile("temp-","");
+        Files.copy(is,tempFile);
+
+
+        is.close();
+        Files.move(tempFile,resultFile);
+
+
+        return resultFile;
+    }
+}
