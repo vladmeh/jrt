2f5186df86554f8750ba182bec544abb4ce57966
VladMeh <vladmeh@gmail.com>
2017-07-12 12:51:04 +0300

    questcollections.level31.lecture04

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task31.task3113;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/* 
+Что внутри папки?
+*/
+public class Solution {
+
+    private static int countDir = 0;
+    private static int countFiles = 0;
+    private static long sizeDir = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Path folder = Paths.get(reader.readLine());
+        reader.close();
+
+        if (Files.isDirectory(folder)) {
+            Files.walkFileTree(folder, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    countFiles++;
+                    sizeDir += attrs.size();
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    if (!dir.equals(folder))
+                        countDir++;
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            System.out.println("Всего папок - " + countDir);
+            System.out.println("Всего файлов - " + countFiles);
+            System.out.println("Общий размер - " + sizeDir);
+        } else {
+            System.out.println(folder.normalize().toAbsolutePath().toString() + " - не папка");
+        }
+
+
+    }
+}
