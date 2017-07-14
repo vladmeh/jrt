6222fb3448574f6abc90650fa67d659b964e75d4
VladMeh <vladmeh@gmail.com>
2017-07-14 11:55:20 +0300

    add Readme

---
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task20.task2028;
+
+import java.util.List;
+
+/* 
+Построй дерево(1)
+*/
+public class CustomTree {
+    public static void main(String[] args) {
+        List<String> list = new CustomTree();
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+        //System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
+        list.remove("5");
+        //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
+    }
+}
