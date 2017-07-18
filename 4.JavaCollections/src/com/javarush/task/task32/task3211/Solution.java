d7877a0fb7bb23250bacf0094ddcf78ffd13b982
VladMeh <vladmeh@gmail.com>
2017-07-18 11:46:55 +0300

    questcollections.level32.lecture06

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task32.task3211;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/* 
+Целостность информации
+*/
+
+public class Solution {
+    public static void main(String... args) throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(new String("test string"));
+        oos.flush();
+        System.out.println(compareMD5(bos, "5a47d12a2e3f9fecf2d9ba1fd98152eb")); //true
+
+    }
+
+    public static boolean compareMD5(ByteArrayOutputStream byteArrayOutputStream, String md5) throws Exception {
+
+        MessageDigest messageDigest;
+        byte[] bytes = new byte[0];
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.reset();
+            messageDigest.update(byteArrayOutputStream.toByteArray());
+            bytes = messageDigest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        BigInteger i = new BigInteger(1, bytes);
+
+        //Все это можно записать в однустрочку:
+        //BigInteger i = new BigInteger(1, messageDigest.digest(byteArrayOutputStream.toByteArray()));
+
+        String result = String.format("%032x", i);
+
+        return result.equals(md5);
+    }
+}
