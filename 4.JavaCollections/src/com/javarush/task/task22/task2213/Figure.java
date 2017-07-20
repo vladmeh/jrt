4a4089116ef310202369333051c8fd943aa1e687
VladMeh <vladmeh@gmail.com>
2017-07-20 15:56:04 +0300

    questcollections.level32.lecture15. Тетрис (8).

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -4,4 +4,24 @@ package com.javarush.task.task22.task2213;
  * @autor mvl on 20.07.2017.
  */
 public class Figure {
+    private int x, y;
+    private int[][] matrix;
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
