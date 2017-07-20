d39b20abfaaefa0c7365513527ba60b26fa2436b
VladMeh <vladmeh@gmail.com>
2017-07-20 16:04:25 +0300

    questcollections.level32.lecture15. Тетрис (10).

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -13,6 +13,24 @@ public class Figure {
         this.matrix = matrix;
     }
 
+    public void left(){}
+
+    public void right(){}
+
+    public void down(){}
+
+    public void up(){}
+
+    public void rotate(){}
+
+    public void downMaximum(){}
+
+    public boolean isCurrentPositionAvailable(){
+        return true;
+    }
+
+    public void landed(){}
+
     public int getX() {
         return x;
     }
