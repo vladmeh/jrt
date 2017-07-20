a1a70973f8735dc9c11c34180855d9977c361153
VladMeh <vladmeh@gmail.com>
2017-07-20 15:23:38 +0300

    questcollections.level32.lecture15. Тетрис (1).

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * @autor mvl on 20.07.2017.
+ */
+public class Field {
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * @autor mvl on 20.07.2017.
+ */
+public class Tetris {
+    public static void main(String[] args) {
+
+    }
+}
