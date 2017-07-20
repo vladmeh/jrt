80d8a3a0df34b3ecbf0e4c04540f3c1710b6b776
VladMeh <vladmeh@gmail.com>
2017-07-20 15:40:31 +0300

    questcollections.level32.lecture15. Тетрис (5).

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -7,8 +7,11 @@ public class Tetris {
     private Field field;
     private Figure figure;
 
-    public static void main(String[] args) {
+    public static Tetris game;
 
+    public static void main(String[] args) {
+        game = new Tetris();
+        game.run();
     }
 
     public void run(){}
