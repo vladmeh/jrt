5b1f47de20418ce82f55932db192eb4fbecf0949
VladMeh <vladmeh@gmail.com>
2017-07-20 15:44:29 +0300

    questcollections.level32.lecture15. Тетрис (6).

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -4,4 +4,24 @@ package com.javarush.task.task22.task2213;
  * @autor mvl on 20.07.2017.
  */
 public class Field {
+    private int width, height;
+    private int[][] matrix;
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new int[height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
