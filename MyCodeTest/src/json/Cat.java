1efc5303ba094db957288a4d36b8a9ce3efe0c2a
VladMeh <vladmeh@gmail.com>
2017-07-21 12:37:39 +0300

    questcollections.level33.lecture03

---
+++ MyCodeTest/src/json/Cat.java
@@ -0,0 +1,45 @@
+package json;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @autor mvl on 21.07.2017.
+ */
+
+@JsonAutoDetect
+class Cat {
+    @JsonProperty("alias")
+    private String name;
+    private int age;
+    @JsonIgnore
+    private int weight;
+
+    Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
---
+++ MyCodeTest/src/json/ConvertToJson.java
@@ -0,0 +1,29 @@
+package json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class ConvertToJson {
+    public static void main(String[] args) throws IOException, IOException {
+        //создание объекта для сериализации в JSON
+        Cat cat = new Cat();
+        cat.setName("Murka");
+        cat.setAge(5);
+        cat.setWeight(4);
+
+        //писать результат сериализации будем во Writer(StringWriter)
+        StringWriter writer = new StringWriter();
+
+        //это объект Jackson, который выполняет сериализацию
+        ObjectMapper mapper = new ObjectMapper();
+
+        // сама сериализация: 1-куда, 2-что
+        mapper.writeValue(writer, cat);
+
+        //преобразовываем все записанное во StringWriter в строку
+        String result = writer.toString();
+        System.out.println(result);
+    }
+}
