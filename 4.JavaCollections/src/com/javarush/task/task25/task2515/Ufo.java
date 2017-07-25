1d7445ec5aa17fd3b78c1049e00e776472c305d1
VladMeh <vladmeh@gmail.com>
2017-07-25 17:16:55 +0300

    questcollections.level33.lecture15. Space(5).

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -4,4 +4,7 @@ package com.javarush.task.task25.task2515;
  * @autor mvl on 25.07.2017.
  */
 public class Ufo extends BaseObject {
+    public Ufo(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
