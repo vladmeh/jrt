55722b6aa86da9fbb549709d633bf130d24919cd
VladMeh <vladmeh@gmail.com>
2017-07-25 17:38:19 +0300

    questcollections.level33.lecture15. Space(7).

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -1,14 +1,22 @@
 package com.javarush.task.task25.task2515;
 
 /**
- * @autor mvl on 25.07.2017.
+ * Базовый класс для всех объектов игры.
  */
 public abstract class BaseObject {
-    private double x, y, radius;
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
+    //состояние объект - жив ли объект
     private boolean isAlive;
 
-    public boolean isAlive() {
-        return isAlive;
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.isAlive = true;
     }
 
     public double getX() {
@@ -35,26 +43,50 @@ public abstract class BaseObject {
         this.radius = radius;
     }
 
-    public BaseObject(double x, double y, double radius) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.isAlive = true;
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    public void draw(Canvas canvas) {
+        //do nothing
     }
 
-    public void draw(){}
-
-    public void move(){}
-
-    public void die(){
-        this.isAlive = false;
+    /**
+     * Двигаем себя на один ход.
+     */
+    public void move() {
+        //do nothing
     }
 
-    public boolean isIntersect(BaseObject o){
-        //расстояние между двумя точками на плоскости
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    public void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public void die() {
+        isAlive = false;
+    }
+
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
+    public boolean isIntersect(BaseObject o) {
         double dx = x - o.x;
         double dy = y - o.y;
-
-        return Math.sqrt(dx * dx + dy * dy) < Math.max(radius, o.radius);
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -4,4 +4,24 @@ package com.javarush.task.task25.task2515;
  * @autor mvl on 25.07.2017.
  */
 public class Canvas {
+    private int width, height;
+    private char[][] matrix;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height][width];
+    }
 }
