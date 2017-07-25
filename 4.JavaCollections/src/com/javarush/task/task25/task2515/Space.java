a973d82889c9fcf94b101804318c50e6351c013d
VladMeh <vladmeh@gmail.com>
2017-07-25 17:11:50 +0300

    questcollections.level33.lecture15. Space(3).

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -1,9 +1,51 @@
 package com.javarush.task.task25.task2515;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @autor mvl on 25.07.2017.
  */
 public class Space {
+    private int width, height;
+    private SpaceShip ship;
+    private ArrayList<Ufo> ufos = new ArrayList<>();
+    private ArrayList<Rocket> rockets = new ArrayList<>();
+    private ArrayList<Bomb> bombs = new ArrayList<>();
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
+    public ArrayList<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public ArrayList<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public ArrayList<Bomb> getBombs() {
+        return bombs;
+    }
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
+    }
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
     public static void main(String[] args) {
 
     }
