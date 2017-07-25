c9b027c0ca9f37888dd3c4e280ca1b7585a3d26d
VladMeh <vladmeh@gmail.com>
2017-07-25 16:53:53 +0300

    questcollections.level33.lecture15. Space(1).

---
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task25.task2515;
+
+/**
+ * @autor mvl on 25.07.2017.
+ */
+public class Space {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task25.task2515;
+
+/**
+ * @autor mvl on 25.07.2017.
+ */
+public class Ufo {
+}
