66447a91e43ce2c7bd5ddae92f352ad886a5b934
VladMeh <vladmeh@gmail.com>
2017-07-26 11:43:55 +0300

    questcollections.level33.lecture15. Space(8).

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -22,6 +22,29 @@ public class Canvas {
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        this.matrix = new char[height][width];
+        this.matrix = new char[height+2][width+2];
+    }
+
+    public void setPoint(double x, double y, char c){
+        int x0 = (int) Math.round(x);
+        int y0 = (int) Math.round(y);
+
+        if (y0 < 0 || y0 > matrix.length) return;
+        if (x0 < 0 || x0 > matrix[0].length) return;
+
+        matrix[y0][x0] = c;
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c){
+        int height = matrix.length;
+        int width = matrix[0].length;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] != 0){
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
     }
 }
