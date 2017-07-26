4d396012b510fd3e5f5aa3ad8f98000151222e53
VladMeh <vladmeh@gmail.com>
2017-07-26 12:30:04 +0300

    questcollections.level33.lecture15. Space(12).

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,18 +1,25 @@
 package com.javarush.task.task25.task2515;
 
 /**
- * @autor mvl on 25.07.2017.
+ * Класс для объектов-ракета
  */
 public class Rocket extends BaseObject {
+
     public Rocket(double x, double y) {
         super(x, y, 1);
     }
 
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
     @Override
     public void draw(Canvas canvas) {
-        canvas.setPoint(x,y,'R');
+        canvas.setPoint(x, y, 'R');
     }
 
+    /**
+     * Двигаем себя вверх на один ход.
+     */
     @Override
     public void move() {
         y--;
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -4,7 +4,17 @@ package com.javarush.task.task25.task2515;
  * @autor mvl on 25.07.2017.
  */
 public class SpaceShip extends BaseObject {
-    public SpaceShip(double x, double y, double radius) {
-        super(x, y, radius);
+    private double dx;
+
+    public SpaceShip(double x, double y) {
+        super(x, y, 3);
+    }
+
+    public void moveLeft() {
+        this.dx = -1;
+    }
+
+    public void moveRight(){
+        this.dx = 1;
     }
 }
