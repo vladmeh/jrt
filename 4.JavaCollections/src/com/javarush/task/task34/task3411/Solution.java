8b43c5a04b74179667a9b1ef4941adba5d42ad43
VladMeh <vladmeh@gmail.com>
2017-08-02 10:06:13 +0300

    questcollections.level34.lecture02.

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3411/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task34.task3411;
+
+/* 
+Ханойские башни
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int count = 3;
+        moveRing('A', 'B', 'C', count);
+    }
+
+    public static void moveRing(char a, char b, char c, int count) {
+        //напишите тут ваш код
+    }
+}
