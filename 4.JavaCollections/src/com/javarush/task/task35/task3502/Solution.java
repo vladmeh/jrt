85c35d10a2be0a6181d0c386423db77356719c3e
VladMeh <vladmeh@gmail.com>
2017-09-06 11:22:29 +0300

    questcollections.level35.full

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3502/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task35.task3502;
+
+/* 
+Знакомство с дженериками
+*/
+public class Solution {
+    public static class SomeClass {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3508/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task35.task3508;
+
+import java.util.List;
+
+/* 
+extends vs super
+*/
+public abstract class Solution {
+    public abstract <T> void one(List destination, List source);
+
+    public abstract <T> void two(List destination, List source);
+
+    public abstract <T> void three(List destination, List source);
+
+    public abstract <T> void four(List destination, List source);
+}
