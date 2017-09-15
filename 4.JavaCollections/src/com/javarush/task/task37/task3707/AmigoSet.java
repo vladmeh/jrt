f196b73890f3fb7384cdcce410fded66a1f29155
VladMeh <vladmeh@gmail.com>
2017-09-15 14:57:37 +0300

    questcollections.level34.lecture15. AmigoSet(3).

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -26,11 +26,31 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
 
     @Override
     public Iterator iterator() {
-        return null;
+        return map.keySet().iterator();
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return map.keySet().contains(o);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.keySet().remove(o);
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
     }
 }
