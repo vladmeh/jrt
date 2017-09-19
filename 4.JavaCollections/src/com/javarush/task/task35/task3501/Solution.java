b72b7c560b81a9215d438ea73b0653f9896076ec
VladMeh <vladmeh@gmail.com>
2017-09-19 12:03:13 +0300

    questcollections.level35.lecture04.

--- 4.JavaCollections/src/com/javarush/task/task35/task3501/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3501/Solution.java
@@ -4,6 +4,6 @@ package com.javarush.task.task35.task3501;
 */
 public class Solution {
     public static void main(String[] args) {
-        Object number = GenericStatic.<Number>someStaticMethod(new Integer(3));
+        Number number = GenericStatic.<Number>someStaticMethod(new Integer(3));
     }
 }
