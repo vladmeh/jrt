555315be6f4a866ca2ebfb6ba73933552d59cb96
VladMeh <vladmeh@gmail.com>
2017-09-19 17:33:39 +0300

    questcollections.level35.lecture10.

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task35.task3507;
 
-import java.util.Set;
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.nio.file.*;
+import java.util.*;
 
 /* 
 ClassLoader - что это такое?
@@ -12,6 +15,55 @@ public class Solution {
     }
 
     public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
-        return null;
+        Set<Animal> set = new HashSet<>();
+
+        File[] list = new File(pathToAnimals).listFiles();
+        for (File file : list) {
+            if (file.isFile() && file.getName().endsWith(".class")) {
+
+                String packageName = Solution.class.getPackage().getName() + ".data"; //some bed solution (Hardcore data)
+                Class clazz = new ClassFromPath().load(file.toPath(), packageName); //Loading class from path
+
+                int score = 0;
+                //find interface Animal
+                Class[] interfaces = clazz.getInterfaces();
+                for (Class interf : interfaces)
+                    if (interf.getSimpleName().equals("Animal")) {
+                        score++;
+                        break;
+                    }
+
+                //Find default constuctor
+                Constructor[] constructors = clazz.getConstructors();
+                for (Constructor constructor : constructors)
+                    if (constructor.getParameterCount() == 0) {
+                        score++;
+                    }
+
+                //if all ok, add to set
+                if (score == 2)
+                    try {
+                        Animal animal = (Animal) clazz.newInstance();
+                        set.add(animal);
+                    } catch (InstantiationException | IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+            }
+        }
+
+        return set;
+    }
+
+    public static class ClassFromPath extends ClassLoader {
+        public Class<?> load(Path path, String packageName) {
+            try {
+                String className = packageName + "." + path.getFileName().toString().replace(".class", "");
+                byte[] b = Files.readAllBytes(path);
+                return defineClass(className, b, 0, b.length); //here main magic
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
