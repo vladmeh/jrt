4b7d3b5acf3b891d09ddc44334de842ce474a994
VladMeh <vladmeh@gmail.com>
2017-12-20 17:57:59 +0300

    QuizFul

---
+++ QuizFul/src/JavaBasics/SolutionTest.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @autor mvl on 20.12.2017.
+ */
+public class SolutionTest {
+    public static void main(String[] args) throws Exception {
+        byte b = 0;
+        while (++b > 0);
+        System.out.println(b);
+    }
+}
