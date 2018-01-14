7a929d562ae32c0a789300b01d6e81740fdc8a2a
vladmeh <vladmeh@gmail.com>
2018-01-14 05:11:49 +0300

    questcollections.level35.lecture15. Factory Method (3).

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -5,7 +5,7 @@ import com.javarush.task.task37.task3702.male.MaleFactory;
 public class Solution {
     public static void main(String[] args) {
         MaleFactory maleFactory = new MaleFactory();
-
+        
         System.out.println(maleFactory.getPerson(99));
         System.out.println(maleFactory.getPerson(4));
         System.out.println(maleFactory.getPerson(15));
---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/female/FemaleFactory.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task37.task3702.female;
+
+import com.javarush.task.task37.task3702.Human;
+
+public class FemaleFactory {
+    public Human getPerson(int age){
+        if (age <= KidGirl.MAX_AGE)
+            return new KidGirl();
+        if (age <= TeenGirl.MAX_AGE)
+            return new TeenGirl();
+        return new Woman();
+    }
+}
