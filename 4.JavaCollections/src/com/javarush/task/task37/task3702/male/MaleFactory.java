7f4f780a2472ca476f6b8acee436d62e0df0b1a2
vladmeh <vladmeh@gmail.com>
2018-01-14 05:00:50 +0300

    questcollections.level35.lecture15. Factory Method (2).

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+public class MaleFactory {
+    public Human getPerson(int age){
+        if (age <= KidBoy.MAX_AGE)
+            return new KidBoy();
+        if (age <= TeenBoy.MAX_AGE)
+            return new TeenBoy();
+        return new Man();
+    }
+}
