b15b232208c4f503184c229fd4c9c93fa29e8bc2
vladmeh <vladmeh@gmail.com>
2018-01-22 00:25:25 +0300

    questcollections.level06.lecture06

--- 4.JavaCollections/src/com/javarush/task/task36/task3602/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3602/Solution.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task36.task3602;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 import java.util.Collections;
+import java.util.List;
 
 /* 
 Найти класс по описанию
@@ -11,6 +15,23 @@ public class Solution {
     }
 
     public static Class getExpectedClass() {
+        Class[] classes = Collections.class.getDeclaredClasses();
+        for (Class clazz: classes){
+            if (Modifier.isPrivate(clazz.getModifiers()) && Modifier.isStatic(clazz.getModifiers())){
+                if (List.class.isAssignableFrom(clazz)){
+                    try {
+                        Constructor constructor = clazz.getDeclaredConstructor();
+                        constructor.setAccessible(true);
+                        List list = (List) constructor.newInstance();
+                        list.get(0);
+                    } catch (IndexOutOfBoundsException e){
+                        return clazz;
+                    } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+                    }
+                }
+            }
+        }
+
         return null;
     }
 }
