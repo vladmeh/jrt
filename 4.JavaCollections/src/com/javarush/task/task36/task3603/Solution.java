812ce25c55605980b1af926444155d089302934f
vladmeh <vladmeh@gmail.com>
2018-01-22 00:33:37 +0300

    questcollections.level06.lecture06

--- 4.JavaCollections/src/com/javarush/task/task36/task3603/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3603/Solution.java
@@ -8,24 +8,18 @@ import java.util.concurrent.CopyOnWriteArrayList;
 Поиск класса по описанию
 */
 public class Solution {
-    public static void main(String... args) {    //it's correct line
-        /*ClassNameToBeReplaced<String> list = new ClassNameToBeReplaced();
+    public static void main(String... args) {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
         list.add("A");
         list.add("B");
         list.add("C");
         list.remove("B");
         List<String> collection = Arrays.asList(new String[]{"B", "C", "D", "B"});
 
-        list.methodNameToBeReplaced(collection);
+        list.addAllAbsent(collection);
 
         for (String string : list) {
             System.out.println(string);
-        }*/
-        /* Expected output
-A
-C
-B
-D
-         */
+        }
     }
 }
